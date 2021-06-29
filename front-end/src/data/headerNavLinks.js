ed011e0f8e3a195f534886248ffa85f47728c4c9
yz3440 <yufeng-zhao@outlook.com>
2021-06-29 13:23:23 +0800

    Remove questions in Header Nav Bar

--- front-end/src/data/headerNavLinks.js
+++ front-end/src/data/headerNavLinks.js
@@ -1,9 +1,8 @@
 const headerNavLinks = [
   { href: '/profile', title: 'Profile' },
   { href: '/data', title: 'Data' },
-  { href: '/questions', title: 'Questions' },
-  { href: '/dashboard', title: 'Dashboard' },
   { href: '/visualize', title: 'Visualize' },
+  { href: '/dashboard', title: 'Dashboard' },
 ];
 
 export default headerNavLinks;
