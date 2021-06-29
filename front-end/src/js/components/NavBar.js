a0dfa6ac6b9e427398f2fbd54109f86387b40026
Almazhan (Alma) Kapan <aa5456@nyu.edu>
2021-06-29 20:03:33 +0800

    Revert "Revert "new files navbar and changes with app.js issue id 93 (#135)" (#140)"
    This reverts commit fee739aa5db86a7c8d9977437a1b3fa4c52ad708.

--- front-end/src/js/components/NavBar.js
+++ front-end/src/js/components/NavBar.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../../App'
+import { FaBars } from 'react-icons/fa'
+import '../../css/components/NavBar.css'
 
-const NavBar = () => {
-  //Feel free to modify code as needed! Just remember that html is inside return statement
-  //and JS code goes before 'return' - exactly where this comment is written
+const Navbar = () => {
   return (
-    <Link to="/Home" className="menu-item">
-      Home
-    </Link>
+    <>
+      <Link className="Nav">
+        <FaBars className="Bars" />
+
+        <div className="NavMenu">
+          <Link className="NavLink" to="/" activeStyle>
+            Humanities Research Lab
+          </Link>
+          <Link className="NavLink" to="/Data" activeStyle>
+            Data
+          </Link>
+          <Link className="NavLink" to="/Visualize" activeStyle>
+            Visualize
+          </Link>
+          <Link className="NavLink" to="/Questions" activeStyle>
+            FAQ
+          </Link>
+          <Link className="NavLink" to="/SampleDataUse" activeStyle>
+            Sample Data Use
+          </Link>
+          <Link className="NavLink" to="/Login" activeStyle>
+            Login
+          </Link>
+          <Link className="NavLink" to="/Signup" activeStyle>
+            SignUp
+          </Link>
+          <Link className="NavLink" to="/Dashboard" activeStyle>
+            Dashboard
+          </Link>
+          <Link className="NavLink" to="/Profile" activeStyle>
+            Profile
+          </Link>
+        </div>
+      </Link>
+    </>
   )
 }
-export default NavBar
+export default Navbar
