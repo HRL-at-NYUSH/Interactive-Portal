7ba7c86fe18f2b7876f7a10f05a9042c31f55158
winnyww <winnyw12138@gmail.com>
2021-06-29 18:49:40 +0800

    upload homepage

--- front-end/src/js/pages/Home.js
+++ front-end/src/js/pages/Home.js
@@ -2,20 +2,41 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import '../../css/pages/Home.css'
+import pic from '../../images-src/home.jpg'
 
 function Home() {
   return (
-    <>
-      <Header />
-      <div className="home">
-        <h2 className="header-text">Humanities Research Lab </h2>
-        <div className="block">
-          <br />
-          <p className="full-text">Home page for Visualization portal!</p>
+    // <>
+    // <Header />
+    <div className="App">
+        <div class="menu-bar">
+          <ul class="breadcrumb" id="left">
+            <li><a href="Home">Humanities Research Lab</a></li>
+          </ul>
+          <ul class="breadcrumb" id="right">
+            <li><a href="data">Data</a></li>
+            <li><a href="visualize">Visualize</a></li>
+            <li><a href="Questions">FAQ</a></li>
+            <li><a href="login">Login</a></li>
+          </ul>
         </div>
-      </div>
-    </>
-  )
-}
 
+        <header className="App-header">
+          <img src={pic} id="background-pic"></img>
+          <h id="header-text">What is HRL?</h>
+          <p id="header-text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </header>
+
+        <div id="decoration"></div>
+
+
+      <div className="App-content">
+        <p>hello</p>
+      </div>
+    </div>
+    // </>
+  );
+}
 export default Home
