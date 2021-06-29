6e2b0f3e29c2742b26d6c345b42ce456622fb750
yz3440 <yufeng-zhao@outlook.com>
2021-06-29 13:24:24 +0800

    migrate global index.css to tailwind.css

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [
+        'ui-sans-serif',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+        'Droid Sans',
+        'Helvetica Neue',
+      ],
+      mono: ['ui-monospace', 'Menlo', 'Monaco', 'Consolas', 'Courier New'],
+    },
   },
   variants: {
     extend: {},
