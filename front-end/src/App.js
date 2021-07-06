37fc13f1708a8c3b0b8f1aa0f9282d3ccae3c1bd
yz3440 <yufeng-zhao@outlook.com>
2021-07-06 12:07:23 +0800

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,31 +1,16 @@
-<<<<<<< HEAD
 import './css/App.css';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './js/pages/home';
-import Data from './js/pages/data';
-import Visualize from './js/pages/visualize';
-import Questions from './js/pages/questions';
-import Profile from './js/pages/profile';
-import Login from './js/pages/login';
-import Signup from './js/pages/signup';
-import SampleDataUse from './js/pages/SampleDataUse';
-import Dashboard from './js/pages/dashboard';
-=======
-import './css/App.css'
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './js/pages/Home.js'
-import Data from './js/pages/Data.js'
-import Visualize from './js/pages/Visualize.js'
-import Questions from './js/pages/Questions.js'
-import Profile from './js/pages/Profile.js'
-import Login from './js/pages/Login.js'
-import Signup from './js/pages/Signup.js'
-import SampleDataUse from './js/pages/SampleDataUse.js'
-import Dashboard from './js/pages/Dashboard'
-import Navbar from './js/components/NavBar.js'
->>>>>>> main
+import Home from './js/pages/Home.js';
+import Data from './js/pages/Data.js';
+import Visualize from './js/pages/Visualize.js';
+import Questions from './js/pages/Questions.js';
+import Profile from './js/pages/Profile.js';
+import Login from './js/pages/Login.js';
+import Signup from './js/pages/Signup.js';
+import SampleDataUse from './js/pages/SampleDataUse.js';
+import Dashboard from './js/pages/Dashboard';
+import Navbar from './js/components/NavBar.js';
 
 import LayoutWrapper from './js/components/LayoutWrapper';
 require('dotenv').config();
@@ -60,7 +45,6 @@ const App = () => {
         */
       >
         <Router>
-<<<<<<< HEAD
           <LayoutWrapper>
             <Switch>
               <Route exact path='/' component={Home} />
@@ -77,38 +61,6 @@ const App = () => {
               <Route path='/questions' component={Questions} />
             </Switch>
           </LayoutWrapper>
-=======
-          <Navbar />
-          <Switch>
-            <Route path="/Data">
-              <Data />
-            </Route>
-            <Route path="/Visualize">
-              <Visualize />
-            </Route>
-            <Route path="/Questions">
-              <Questions />
-            </Route>
-            <Route path="/Profile">
-              <Profile />
-            </Route>
-            <Route path="/Login">
-              <Login />
-            </Route>
-            <Route path="/Signup">
-              <Signup />
-            </Route>
-            <Route path="/SampleDataUse">
-              <SampleDataUse />
-            </Route>
-            <Route path="/Dashboard">
-              <Dashboard />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
->>>>>>> main
         </Router>
       </AuthContext.Provider>
     </div>
