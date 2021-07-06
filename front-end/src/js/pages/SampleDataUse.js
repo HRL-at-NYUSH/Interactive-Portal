dff5812f3178bd01ebd3000ec6bef9634e2547ec
yz3440 <yufeng-zhao@outlook.com>
2021-07-06 16:12:04 +0800

    add histogram sample chart

--- front-end/src/js/pages/SampleDataUse.js
+++ front-end/src/js/pages/SampleDataUse.js
@@ -1,6 +1,9 @@
 import React from 'react';
 //Important! Below the mock data is imported from the utils folder
 import { dataGeo, dataNonGeo } from '../utils/MockData.js';
+import Histogram from '../../graphs/Histogram';
+import { ChevronDownIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
 
 //Main component that displays the data and renders page html elements
 //Components in React can be in the form of functions, classes etc.
@@ -10,6 +13,9 @@ function SampleDataUse() {
   // See example below inside the DataDisplay component
   const titleNonGeo = 'Using non-geospatial data ';
   const titleGeo = 'Using geospatial data ';
+
+  const dataKeys = Object.keys(dataNonGeo[0]);
+  const [histoXAttr, setHistoXAttr] = useState('ID');
   //Below is the html code (return value)
   return (
     <>
@@ -18,7 +24,37 @@ function SampleDataUse() {
           A sample usage of the mock data and react components
         </HeaderText>
 
-        <div className=''>
+        <div className='flex flex-wrap justify-between'>
+          <div className='rounded overflow-hidden shadow-lg m-auto p-4'>
+            <Histogram
+              data={dataNonGeo}
+              xAxisAttribute={histoXAttr}
+              title={`People from Different ${histoXAttr}`}
+            ></Histogram>
+            <div className=' text-center border-t-2 px-6 py-2'>
+              <div className=' font-bold text-xl py-2'>
+                Histogram of Non-Geo Data
+              </div>
+              <div className='inline-block relative w-64'>
+                <select
+                  className='block appearance-none w-full h-9 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'
+                  onChange={(e) => setHistoXAttr(e.target.value)}
+                >
+                  {dataKeys.map((key) => (
+                    <option key={key} value={key}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+                <div className='w-9 h-9 pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+                  <ChevronDownIcon></ChevronDownIcon>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
           <DataDisplay data={dataNonGeo} title={titleNonGeo} />
           <DataDisplay data={dataGeo} title={titleGeo} />
           <br />
