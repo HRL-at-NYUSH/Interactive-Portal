263c4778a140dcc07cf3ed40b40e5ce924bf1d07
Almazhan Kapan <aa5456@nyu.edu>
2021-07-07 07:57:49 +0800

    add syntax fixes

--- front-end/src/js/components/dashboard-tabs/DashPanel.js
+++ front-end/src/js/components/dashboard-tabs/DashPanel.js
@@ -4,7 +4,7 @@ import TabItem from '../dashboard-tabs/TabItem'
 import DropNav from '../dropdown/DropNav'
 import Item from '../dropdown/DropItem'
 import { FaPlus } from 'react-icons/fa'
-import {AiFillCloseSquare} from 'react-icons/ai'
+import { AiFillCloseSquare } from 'react-icons/ai'
 import '../../../css/components/Dropdown.css'
 import Distribution from '../dashboard-tabs/Distribution'
 import Time from '../dashboard-tabs/Time'
@@ -12,114 +12,166 @@ import Space from '../dashboard-tabs/Space'
 import Playground from '../dashboard-tabs/Playground'
 import Correlation from '../dashboard-tabs/Correlation'
 
-let dcount = 0;
-let tcount = 0;
-let scount = 0;
-let ccount = 0;
-let pcount = 0;
-let overallcount = 2;
+let dcount = 0
+let tcount = 0
+let scount = 0
+let ccount = 0
+let pcount = 0
+let overallcount = 2
 
 class DashPanel extends React.Component {
-
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      tabitems: [{label: "Distribution", element: <Distribution />},{label: "Time", element: <Time />}],
+      tabitems: [
+        { label: 'Distribution', element: <Distribution /> },
+        { label: 'Time', element: <Time /> },
+      ],
     }
   }
 
   handleStateChange = (entry) => {
     if (isNaN(entry)) {
       if (overallcount > 5) {
-        alert("Sorry! Too many tabs");
-      }
-      else if (entry == "Distribution") {
-        this.setState(prevState => ({
-          tabitems: [...prevState.tabitems, {label: (entry + " " + dcount), element: <Distribution /> }]
+        alert('Sorry! Too many tabs')
+      } else if (entry === 'Distribution') {
+        this.setState((prevState) => ({
+          tabitems: [
+            ...prevState.tabitems,
+            { label: entry + ' ' + dcount, element: <Distribution /> },
+          ],
         }))
-        dcount += 1;
-        overallcount += 1;
-      } else if (entry == "Time") {
-        this.setState(prevState => ({
-          tabitems: [...prevState.tabitems, {label: (entry + " " + tcount), element: <Time />}]
+        dcount += 1
+        overallcount += 1
+      } else if (entry === 'Time') {
+        this.setState((prevState) => ({
+          tabitems: [
+            ...prevState.tabitems,
+            { label: entry + ' ' + tcount, element: <Time /> },
+          ],
         }))
-        tcount += 1;
-        overallcount += 1;
-      }
-      else if (entry == "Space") {
-        this.setState(prevState => ({
-          tabitems: [...prevState.tabitems, {label: (entry + " " + scount), element: <Space />}]
+        tcount += 1
+        overallcount += 1
+      } else if (entry === 'Space') {
+        this.setState((prevState) => ({
+          tabitems: [
+            ...prevState.tabitems,
+            { label: entry + ' ' + scount, element: <Space /> },
+          ],
         }))
-        scount += 1;
-        overallcount += 1;
-      }
-      else if (entry == "Correlation") {
-        this.setState(prevState => ({
-          tabitems: [...prevState.tabitems, {label: (entry + " " + ccount), element:<Correlation />}]
+        scount += 1
+        overallcount += 1
+      } else if (entry === 'Correlation') {
+        this.setState((prevState) => ({
+          tabitems: [
+            ...prevState.tabitems,
+            { label: entry + ' ' + ccount, element: <Correlation /> },
+          ],
         }))
-        ccount += 1;
-        overallcount += 1;
-      }
-      else if (entry == "Playground") {
-        this.setState(prevState => ({
-          tabitems: [...prevState.tabitems, {label: (entry + " " + pcount), element: <Playground />}]
+        ccount += 1
+        overallcount += 1
+      } else if (entry === 'Playground') {
+        this.setState((prevState) => ({
+          tabitems: [
+            ...prevState.tabitems,
+            { label: entry + ' ' + pcount, element: <Playground /> },
+          ],
         }))
-        pcount += 1;
-        overallcount += 1;
+        pcount += 1
+        overallcount += 1
       }
-    } else if (typeof(entry) == 'number' && overallcount > 1) {
-      this.setState(prevState => ({
-        tabitems: [...prevState.tabitems.slice(0, entry), ...prevState.tabitems.slice(entry + 1)]
+    } else if (typeof entry === 'number' && overallcount > 1) {
+      this.setState((prevState) => ({
+        tabitems: [
+          ...prevState.tabitems.slice(0, entry),
+          ...prevState.tabitems.slice(entry + 1),
+        ],
       }))
-      overallcount -= 1;
+      overallcount -= 1
     }
-    console.log(overallcount);
+    console.log(overallcount)
   }
 
-  render(){
-
+  render() {
     return (
       <>
-      <div className="block">
-        <div>
-          <DropNav>
-            <Item icon={<FaPlus />}>
-              <div className="dropdown">
-                <div>
-                  <a href="#" className="menu-item" onClick={() => this.handleStateChange("Distribution")}>Distribution</a>
+        <div className="block">
+          <div>
+            <DropNav>
+              <Item icon={<FaPlus />}>
+                <div className="dropdown">
+                  <div>
+                    <a
+                      href="#"
+                      className="menu-item"
+                      onClick={() => this.handleStateChange('Distribution')}
+                    >
+                      Distribution
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="#"
+                      className="menu-item"
+                      onClick={() => this.handleStateChange('Correlation')}
+                    >
+                      Correlation
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="#"
+                      className="menu-item"
+                      onClick={() => this.handleStateChange('Time')}
+                    >
+                      Time
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="#"
+                      className="menu-item"
+                      onClick={() => this.handleStateChange('Space')}
+                    >
+                      Space
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="#"
+                      className="menu-item"
+                      onClick={() => this.handleStateChange('Playground')}
+                    >
+                      Playground
+                    </a>
+                  </div>
                 </div>
-                <div>
-                  <a href="#" className="menu-item" onClick={() => this.handleStateChange("Correlation")} >Correlation</a>
-                </div>
-                <div>
-                <a href="#" className="menu-item" onClick={() => this.handleStateChange("Time")}>Time</a>
-                </div>
-                <div>
-                  <a href="#" className="menu-item" onClick={() => this.handleStateChange("Space")}>Space</a>
-                </div>
-                <div>
-                  <a href="#" className="menu-item" onClick={() => this.handleStateChange("Playground")}>Playground</a>
-                </div>
-              </div>
-            </Item>
-          </DropNav>
+              </Item>
+            </DropNav>
+          </div>
+          <div className="tab-container">
+            <DashTabs>
+              {this.state.tabitems.map((d, i) => (
+                <TabItem
+                  label={d.label}
+                  close={
+                    <AiFillCloseSquare
+                      className="close"
+                      id={i}
+                      onClick={() => this.handleStateChange(i)}
+                    />
+                  }
+                >
+                  {d.element}
+                </TabItem>
+              ))}
+            </DashTabs>
+          </div>
         </div>
-        <div className="tab-container">
-        <DashTabs>
-        {this.state.tabitems.map((d, i) => (
-          <TabItem label={d.label} close={<AiFillCloseSquare className="close" id={i} onClick={() => this.handleStateChange(i)}/>}>
-          {d.element}
-          </TabItem>
-        ))}
-        </DashTabs>
-        </div>
-      </div>
       </>
     )
-
   }
 }
 
 export default DashPanel
-
