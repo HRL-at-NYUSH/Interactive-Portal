8de0c4e0f10cc4b1fe6547d09270f9f54869469d
Almazhan Kapan <aa5456@nyu.edu>
2021-07-09 04:59:44 +0800

    mock data update

--- back-end/models/entity.js
+++ back-end/models/entity.js
@@ -1,3 +1,45 @@
 const mongoose = require('mongoose')
 
-const EntitySchema = new mongoose.Schema({})
+const entitySchema = new mongoose.Schema({
+  ID: {
+    type: Number,
+    required: true,
+    unique: true,
+    validate: {
+      validator: Number.isInteger,
+      message: '{VALUE} is not an integer value',
+    },
+  },
+  Time: {
+    type: Number,
+    required: true,
+  },
+  Person: { type: Boolean, required: true },
+  Company: { type: Boolean, required: true },
+  Name: { type: String, required: true },
+  Occupation: { type: String, required: true },
+  Address: { type: String, required: true },
+  Birthplace: { type: String, required: true },
+  State: { type: String, required: true },
+  Gender: { type: String, required: true },
+  Age: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: Number.isInteger,
+      message: '{VALUE} is not an integer value',
+    },
+  },
+  Immigration_year: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: Number.isInteger,
+      message: '{VALUE} is not an integer value',
+    },
+  },
+  Latitude: { type: Number, required: true },
+  Longitude: { type: Number, required: true },
+})
+
+module.exports = mongoose.model('Entity', entitySchema)
