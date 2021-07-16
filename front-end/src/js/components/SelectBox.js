2ae3294b8540d1b84b4b5a22519d91cf751ba13e
yz3440 <yufeng-zhao@outlook.com>
2021-07-16 15:27:25 +0800

    fix SelectBox style again

--- front-end/src/js/components/SelectBox.js
+++ front-end/src/js/components/SelectBox.js
@@ -16,7 +16,7 @@ const onValueChange = ()=>{
 
 export default function SelectBox({ data, onValueChange }) {
   return (
-    <div className='inline-block relative'>
+    <div className='inline-block relative w-full'>
       <select
         className='block appearance-none w-full h-9 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'
         onChange={(e) => onValueChange(e.target.value)}
