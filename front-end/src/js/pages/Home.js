53256e56a750e1b3ef93d397215a59156a284e37
apple <apple@appledeiMac.local>
2021-07-19 11:10:19 +0800

    update homepage design

--- front-end/src/js/pages/Home.js
+++ front-end/src/js/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { useEffect } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import '../../css/pages/Home.css'
 import pic from '../../images-src/home.jpg'
-import test from '../../images-src/test.jpeg'
+import logo from '../../images-src/logo3.png'
 import headshot1 from '../../images-src/headshot-1.jpg'
 import headshot2 from '../../images-src/headshot-2.jpg'
 import headshot3 from '../../images-src/headshot-3.jpg'
@@ -13,117 +14,164 @@ import headshot6 from '../../images-src/headshot-6.jpg'
 import highlight1 from '../../images-src/highlight-1.jpg'
 import highlight2 from '../../images-src/highlight-2.jpg'
 import highlight3 from '../../images-src/highlight-3.jpg'
+import highlight4 from '../../images-src/highlight-4.jpg'
+import diagram1 from '../../images-src/diagram-1.jpeg'
+import diagram2 from '../../images-src/diagram-2.jpeg'
+import diagram3 from '../../images-src/diagram-3.jpeg'
+import diagram4 from '../../images-src/diagram-4.jpeg'
+import diagram5 from '../../images-src/diagram-5.jpeg'
+
+
 
 function Home() {
+  // useEffect(() => {
+  //   const scriptTag = document.createElement('script');
+
+  //   scriptTag.src = "https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.pkgd.js";
+  //   scriptTag.async = true;
+
+  //   document.body.appendChild(scriptTag);
+  //   return () => {
+  //       document.body.removeChild(scriptTag);
+  //   }
+  // }, []);
+
+
   return (
     <div className="App">
-        <div class="menu-bar">
-          <ul class="breadcrumb" id="left">
-            <li><a href="Home">Humanities Research Lab</a></li>
-          </ul>
-          <ul class="breadcrumb" id="right">
-            <li><a href="data">Data</a></li>
-            <li><a href="visualize">Visualize</a></li>
-            <li><a href="Questions">FAQ</a></li>
-            <li><a href="login">Login</a></li>
-          </ul>
-        </div>
-
         <header className="App-header">
-          <img src={pic} id="background-pic"></img>
-          <h id="header-text">What is HRL?</h>
+          {/* <img src={pic} id="background-pic"></img> */}
+          <h id="header-text">History beyond the New York City</h>
           <p id="header-text">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
+          History Beyond gives undergraduate students experiences in undertaking independent historical research. The course encourages students to think creatively and boldly, to negotiate past difficulties in collaboration, and to learn from setbacks.</p>
         </header>
 
-        <div id="decoration"></div>
+        <div id="decoration">
+          <img src={logo} id='logo'></img>
+        </div>
 
-        <div id="title"><h>Data Literacy & Mission</h></div>
+        <div id="title"><h1>What is HRL?</h1></div>
         <div className="App-content">
           <div><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p></div>
           <div><p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p></div>
         </div>
 
-        <div id="title"><h>Course & Highlights</h></div>
-
+        <div id="title"><h1>Course & Highlights</h1></div>
         <div className="App-content-2">
-          <div>
-            <img src={highlight1} id='eventImg'></img>
-            <div id="event-info">
-              <h1>revisit the history</h1>
-              <p1> those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et.</p1>
-            </div>
-          </div>
-          <div>
-            <img src={highlight2} id='eventImg'></img>
-            <div id="event-info">
-              <h1>man of the time</h1>
-              <p1> those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et.</p1>
-            </div>
-          </div>
-          <div><img src={highlight3} id='eventImg-half'></img></div>
+              <div class="gallery-cell">
+                <img src={highlight1} id='eventImg'></img>
+                <div id="event-info">
+                  <h2>revisit the history</h2>
+                  <p1> those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et.</p1>
+                </div>
+              </div>
+              <div class="gallery-cell">
+                <img src={highlight2} id='eventImg'></img>
+                <div id="event-info">
+                  <h2>man of the time</h2>
+                  <p1> those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et.</p1>
+                </div>
+              </div>
+              <div class="gallery-cell">
+                <img src={highlight3} id='eventImg'></img>
+                <div id="event-info">
+                  <h2>revisit the history</h2>
+                  <p1> those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et.</p1>
+                </div>
+              </div>
+              <div class="gallery-cell">
+                <img src={highlight4} id='eventImg'></img>
+                <div id="event-info">
+                  <h2>man of the time</h2>
+                  <p1> those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et.</p1>
+                </div>
+              </div> 
         </div>
 
-
-
-        <div id="decoration-2">see more ></div>
-
-
-
-
-        <div id="title-change"><h>Development Info</h></div>
+        <div id="title"><h1>Development & More</h1></div>
 
         <div className="App-content-3">
-          <div>
-            <img src={test} id="test"></img>
+          <div id="diagram-cell">
+            <img src={diagram1} id='diagram'></img>
+            <div id="develop-info">
+              <h3>23</h3>
+              <p>datasets</p>
+            </div>
+          </div>
+          <div id="diagram-cell">
+            <img src={diagram2} id='diagram'></img>
+            <div id="develop-info">
+              <h3>10</h3>
+              <p> publications</p>
+            </div>
+          </div>
+          <div id="diagram-cell">
+            <img src={diagram3} id='diagram'></img>
+            <div id="develop-info">
+              <h3>5</h3>
+              <p> universities</p>
+            </div>
+          </div>
+          <div id="diagram-cell">
+            <img src={diagram4} id='diagram'></img>
+            <div id="develop-info">
+              <h3>51</h3>
+              <p> contributors</p>
+            </div>
+          </div>
+          <div id="diagram-cell">
+            <img src={diagram5} id='diagram'></img>
+            <div id="develop-info">
+              <h3>68</h3>
+              <p> platforms</p>
+            </div>
           </div>
         </div>
 
 
 
-        <div id="title"><h>Team Members</h></div>
+        <div id="title"><h1>Team Members</h1></div>
 
         <div className="App-content-4">
           <div id="profile">
             <img src={headshot1} id='headshot'></img>
             <div id="introduction">
-              <h1>Carmen</h1>
+              <h2>Carmen</h2>
               <p1> Data Scientist</p1>
             </div>
           </div>
           <div id="profile">
             <img src={headshot2} id='headshot'></img>
             <div id="introduction">
-              <h1>Bob</h1>
+              <h2>Bob</h2>
               <p1> Decision Support Analyst</p1>
             </div>
           </div>
           <div id="profile">
             <img src={headshot3} id='headshot'></img>
             <div id="introduction">
-              <h1>Lisa</h1>
+              <h2>Lisa</h2>
               <p1>Network Management Specialist</p1>
             </div>
           </div>
           <div id="profile">
             <img src={headshot4} id='headshot'></img>
             <div id="introduction">
-              <h1>Alex</h1>
+              <h2>Alex</h2>
               <p1> Software Product Manager</p1>
             </div>
           </div>
           <div id="profile">
             <img src={headshot5} id='headshot'></img>
             <div id="introduction">
-              <h1>Cara</h1>
+              <h2>Cara</h2>
               <p1> UX Designer</p1>
             </div>
           </div>
           <div id="profile">
             <img src={headshot6} id='headshot'></img>
             <div id="introduction">
-              <h1>Jenny</h1>
+              <h2>Jenny</h2>
               <p1>Tech Intern</p1>
             </div>
           </div>
@@ -136,5 +184,7 @@ function Home() {
         </div>
     </div>
   );
+
 }
+
 export default Home
