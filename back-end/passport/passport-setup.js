74117a81750c8fc49bf4f895b0aad60b062fe88d
Kai Elwood-Dieu <kaielwooddieu@gmail.com>
2021-07-19 00:49:00 -0500

    added backend functionality, need to connect to DB

---
+++ back-end/passport/passport-setup.js
@@ -0,0 +1,38 @@
+var passport = require("passport");
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+passport.serializeUser(function (user, done) {
+  console.log("serialized", user);
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  //   User.findById(id, function (err, user) {
+  //     console.log("serialized", id);
+  //     done(err, user);
+  //   });
+  done(null, user);
+});
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a token, tokenSecret, and Google profile), and
+//   invoke a callback with a user object.
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "838596502010-0p0p11r3amea5qojgot7j52n2pb4saf7.apps.googleusercontent.com",
+      clientSecret: "BIAryS6XKkEVzhsqLzitJeOD",
+      callbackURL: "http://localhost:4000/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      console.log("this is working", profile);
+      //   console.log(err);
+      return done(null, profile);
+      // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      //   return done(err, user);
+      // });
+    }
+  )
+);
