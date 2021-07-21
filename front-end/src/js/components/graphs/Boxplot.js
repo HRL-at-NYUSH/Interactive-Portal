4023f3c87e1859fcc2734cefc6c1fc2f1ee026ee
Almazhan (Alma) Kapan <aa5456@nyu.edu>
2021-07-21 16:45:38 +0800

    Issue 55 menu toolbar1 (#175)
    * add toggles
    
    * add libs and fix tabs
    
    * menu toolbar front-end
    
    * menu toolbar front-end final update

--- front-end/src/js/components/graphs/Boxplot.js
+++ front-end/src/js/components/graphs/Boxplot.js
@@ -4,13 +4,13 @@ import '../../../css/components/Graph.css'
 //temporary
 
 class Boxplot extends React.Component {
-    render() {
-        return (
-            <div className="graph">
-                <p>Boxplot</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="graph">
+        <p className="graph-title">Boxplot</p>
+      </div>
+    )
+  }
 }
 
 export default Boxplot
--- front-end/src/js/pages/Dashboard.js
+++ front-end/src/js/pages/Dashboard.js
@@ -3,14 +3,18 @@ import DashPanel from '../components/dashboard-tabs/DashPanel'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import '../../css/pages/Dashboard.css'
+import Menu from '../components/Menu'
+import Toggle from '../components/Toggle'
+import './styles.css'
 
 function Dashboard() {
   // const { category } = useParams()
   return (
     <>
-      <div>
-        <h2 className="header-text">Dashboard page </h2>
-        <DashPanel />
+      <div className="dash-block">
+        <div className="rows">
+          <DashPanel />
+        </div>
       </div>
     </>
   )
