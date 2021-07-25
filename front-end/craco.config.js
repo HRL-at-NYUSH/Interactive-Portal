0358bab7ca77ffd5a6b9f713e49330bd9f49452a
yz3440 <yufeng-zhao@outlook.com>
2021-07-25 12:23:39 +0800

    add craco alias for easier imports

--- front-end/craco.config.js
+++ front-end/craco.config.js
@@ -1,4 +1,23 @@
+const CracoAlias = require('craco-alias');
+
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'options',
+        baseUrl: './',
+        aliases: {
+          '@components': './src/components',
+          '@pages': './src/pages',
+
+          // you can alias packages too
+          // "@material-ui": "./node_modules/@material-ui-ie10"
+          // "@file": "./src/file.js",
+        },
+      },
+    },
+  ],
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
