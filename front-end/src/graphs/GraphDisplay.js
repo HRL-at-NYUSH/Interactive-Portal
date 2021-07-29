020b74d72f99593990e4ee3e55dce34129bd4b42
yz3440 <yufeng-zhao@outlook.com>
2021-07-29 21:17:52 +0800

    build basic box plot

--- front-end/src/graphs/GraphDisplay.js
+++ front-end/src/graphs/GraphDisplay.js
@@ -13,7 +13,6 @@ import HeatMap from './HeatMap';
 import LineGraph from './LineGraph';
 import ScatterPlot from './ScatterPlot';
 import StackedBarChart from './StackedBarChart';
-import './GraphDisplay.css';
 
 import { ChevronDownIcon } from '@heroicons/react/solid';
 import { useState } from 'react';
@@ -26,17 +25,21 @@ function GraphDisplay() {
   const selectBoxData = dataKeys.map((d) => ({ fieldName: d, value: d }));
 
   const [histoXAttr, setHistoXAttr] = useState('ID');
+
+  const [boxPlotXAttr, setBoxPlotXAttr] = useState('ID');
+  const [boxPlotYAttr, setBoxPlotYAttr] = useState('ID');
+
   //Below is the html code (return value)
   return (
     <>
       <div>
         <HeaderText>A page that displays your graph component</HeaderText>
 
-        <div className='flex flex-wrap justify-between'>
-          <div className='rounded-lg overflow-hidden shadow-lg m-auto p-4 bg-white'>
-            {
-              //Put your graph component below the Histogram Component
-            }
+        <div className='flex flex-col justify-between'>
+          {/* Put your graph component below the Histogram Card */}
+
+          {/* HISTOGRAM CARD */}
+          <div className='rounded-lg overflow-hidden shadow-lg m-auto mb-4 p-4 bg-white'>
             <div className='w-1/2 text-3xl text-center font-bold border-b-2 p-2 m-auto'>
               Histogram
             </div>
@@ -54,6 +57,36 @@ function GraphDisplay() {
                   data={selectBoxData}
                   onValueChange={setHistoXAttr}
                 ></SelectBox>
+              </div>
+            </div>
+          </div>
+
+          <div className='rounded-lg overflow-hidden shadow-lg m-auto p-4 bg-white'>
+            <div className='w-1/2 text-3xl text-center font-bold border-b-2 p-2 m-auto'>
+              Box Plot
+            </div>
+            <BoxPlot
+              data={dataNonGeo}
+              xAxisAttribute={boxPlotXAttr}
+              yAxisAttribute={boxPlotYAttr}
+              title={`People with Different ${boxPlotYAttr} in Different ${boxPlotXAttr} Groups`}
+            ></BoxPlot>
+
+            <div className='text-center border-t-2 py-2'>
+              <div className='font-bold text-xl py-2'>
+                BoxPlot of Non-Geo Data
+              </div>
+              <div className='m-auto w-64'>
+                Set X Attribute
+                <SelectBox
+                  data={selectBoxData}
+                  onValueChange={setBoxPlotXAttr}
+                />
+                Set Y Attribute
+                <SelectBox
+                  data={selectBoxData}
+                  onValueChange={setBoxPlotYAttr}
+                />
               </div>
             </div>
           </div>
