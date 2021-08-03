9ab86750785c5036a36a746dcd593f3e5b603ebe
saraaahh63 <sat454@nyu.edu>
2021-08-03 15:07:41 +0300

    graph modal component

---
+++ front-end/src/graphs/GraphModal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class GraphModal extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          graph: null,
+        };
+      }
+
+      
+
+}
