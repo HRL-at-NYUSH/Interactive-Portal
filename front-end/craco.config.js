a6cd104495f57bcae0336bcc26be90bdfe67d290
yz3440 <yufeng-zhao@outlook.com>
2021-08-10 12:25:04 +0800

    fix import intellisense with alias

--- front-end/craco.config.js
+++ front-end/craco.config.js
@@ -5,20 +5,10 @@ module.exports = {
     {
       plugin: CracoAlias,
       options: {
-        source: 'options',
-        baseUrl: './',
-        aliases: {
-          '@components': './src/components',
-          '@pages': './src/pages',
-          '@data': './src/data',
-          '@assets': './src/assets',
-          '@graphs': './src/graphs',
-          '@utils': './src/utils',
-
-          // you can alias packages too
-          // "@material-ui": "./node_modules/@material-ui-ie10"
-          // "@file": "./src/file.js",
-        },
+        source: 'jsconfig',
+        // baseUrl SHOULD be specified
+        // plugin does not take it from jsconfig
+        baseUrl: '.',
       },
     },
   ],
