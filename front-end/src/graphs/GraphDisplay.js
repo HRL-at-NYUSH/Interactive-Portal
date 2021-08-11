f70c31930588b92e97f67678e79233bfe1087d8b
winnyww <winnyw12138@gmail.com>
2021-08-11 23:53:21 +0800

    graph update WITH CONFLICT

--- front-end/src/graphs/GraphDisplay.js
+++ front-end/src/graphs/GraphDisplay.js
@@ -1,7 +1,9 @@
 import React from "react";
 //Important! Below the mock data is imported from the utils folder
-import { dataGeo, dataNonGeo } from "../js/utils/MockData.js";
+import { dataGeo, dataNonGeo } from "../js/utils/MockData-Winny.js";
 import Histogram from "./Histogram";
+// import HeaderText from '@components/HeaderText.js';
+// import SelectBox from '@components/SelectBox.js';
 import BarChart from "./BarChart";
 import BoxPlot from "./BoxPlot";
 import Correlation from "./Correlation";
@@ -15,6 +17,13 @@ import "./GraphDisplay.css";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import { useState } from "react";
 
+function selectOnce(checkbox) {
+  var checkboxes = document.getElementsByName('game')
+  checkboxes.forEach((item) => {
+    if (item !== checkbox) item.checked =  false
+  })
+}
+
 //Main component that displays your created graph
 //Components in React can be in the form of functions, classes etc.
 //and consist of both javascript code and html code (html is the return value)
@@ -23,14 +32,17 @@ function GraphDisplay() {
   const [histoXAttr, setHistoXAttr] = useState("ID");
 
   //scatter plot attributes
-  const [ScatoXAttr, setScatoXAttr] = useState("ID");
-  const [ScatoYAttr, setScatoYAttr] = useState("ID");
+  const [ScatoXAttr, setScatoXAttr] = useState("");
+  const [ScatoYAttr, setScatoYAttr] = useState("");
+  const [ScatoColor, setScatoColor] = useState("RACE");
+  const [ScatoLine, setScatoLine] = useState("");
+  const [ScatoBg, setScatoBg] = useState("");
 
   //Below is the html code (return value)
   return (
     <>
       <div>
-        <HeaderText>A page that displays your graph component</HeaderText>
+        {/* <HeaderText>A page that displays your graph component</HeaderText> */}
 
         <div className="flex flex-wrap justify-between">
           <div className="rounded overflow-hidden shadow-lg m-auto p-4">
@@ -66,57 +78,73 @@ function GraphDisplay() {
             </div>
           </div>
 
-          <div className="rounded overflow-hidden shadow-lg m-auto p-4">
-            <p>Scatter Plot</p>
-            <ScatterPlot
-              data={dataNonGeo}
-              xAxisAttribute={ScatoXAttr}
-              yAxisAttribute={ScatoYAttr}
-            ></ScatterPlot>
-            <div className="flex space-x-2 text-center border-t-2 px-1 py-2">
-              <div class="flex-1">
-                <div className="font-bold text-xl py-2">X Axis</div>
-                <div className="inline-block relative w-60">
-                  <select
-                    className="block appearance-none w-full h-10 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                    onChange={(e) => setScatoXAttr(e.target.value)}
-                  >
-                    {dataKeys.map((key) => (
-                      <option key={key} value={key}>
-                        {key}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-              <div class="flex-1">
-                <div className="font-bold text-xl py-2">Y Axis</div>
-                <div className="inline-block relative w-60">
-                  <select
-                    className="block appearance-none w-full h-10 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                    onChange={(e) => setScatoYAttr(e.target.value)}
-                  >
-                    {dataKeys.map((key) => (
-                      <option key={key} value={key}>
-                        {key}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
+          <div className="flex rounded-lg overflow-hidden shadow-lg m-auto p-4">
+            <div className="flex-auto">
+              <p>Scatter Plot</p>
+              <ScatterPlot
+                data={dataNonGeo}
+                xAxisAttribute={ScatoXAttr}
+                yAxisAttribute={ScatoYAttr}
+                colorAttribute={ScatoColor}
+                lineAttribute={ScatoLine}
+                backgroundAttribute={ScatoBg}
+              ></ScatterPlot>
             </div>
+            <div className="flex-auto text-left">
+              <div className="flex flex-row mx-16 my-10">
+                <div class="space-x-2 mx-5">
+                  <div className="font-bold text-xl py-2">X Axis</div>
+                  <div className="relative">
+                    <form>
+                      <input type="radio" name="game" class="mx-2" value={dataKeys[16]} onclick="selectOnce(this)" onChange={(e) => setScatoXAttr(e.target.value)}></input>{dataKeys[16]}<br></br>
+                      <input type="radio" name="game" class="mx-2" value={dataKeys[22]} onclick="selectOnce(this)" onChange={(e) => setScatoXAttr(e.target.value)}></input>{dataKeys[22]}<br></br>
+                      <input type="radio" name="game" class="mx-2" value={dataKeys[21]} onclick="selectOnce(this)" onChange={(e) => setScatoXAttr(e.target.value)}></input>{dataKeys[21]}<br></br>
+                      <input type="radio" name="game" class="mx-2" value={dataKeys[27]} onclick="selectOnce(this)" onChange={(e) => setScatoXAttr(e.target.value)}></input>{dataKeys[27]}<br></br>
+                    </form>
+                  </div>
+                </div>
+                <div class="space-x-2 mx-5">
+                <div className="font-bold text-xl py-2">Y Axis</div>
+                <div className="relative">
+                  <form>
+                    <input type="radio" name="game" class="mx-2" value={dataKeys[16]} onclick="selectOnce(this)" onChange={(e) => setScatoYAttr(e.target.value)}></input>{dataKeys[16]}<br></br>
+                    <input type="radio" name="game" class="mx-2" value={dataKeys[22]} onclick="selectOnce(this)" onChange={(e) => setScatoYAttr(e.target.value)}></input>{dataKeys[22]}<br></br>
+                    <input type="radio" name="game" class="mx-2" value={dataKeys[21]} onclick="selectOnce(this)" onChange={(e) => setScatoYAttr(e.target.value)}></input>{dataKeys[21]}<br></br>
+                    <input type="radio" name="game" class="mx-2" value={dataKeys[27]} onclick="selectOnce(this)" onChange={(e) => setScatoYAttr(e.target.value)}></input>{dataKeys[27]}<br></br>
+                  </form>
+                 </div>
+              </div>
+              </div>
+              {/* <div className="text-center my-5"><input type="range" min="1" max="500" value="50"></input></div> */}
+              <div className="text-center my-5"><input type="range" min="1" max="500" value="50"></input></div>
+              <div className="px-20 py-8">
+                <select
+                  className="block appearance-none h-9 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  onChange={(e) => setScatoColor(e.target.value)}
+                >
+                    <option key={dataKeys[4]} value={dataKeys[4]}>{dataKeys[4]}</option>
+                    <option key={dataKeys[3]} value={dataKeys[3]}>{dataKeys[3]}</option>
+                    <option key={dataKeys[8]} value={dataKeys[8]}>{dataKeys[8]}</option>
+                    <option key={dataKeys[15]} value={dataKeys[15]}>{dataKeys[15]}</option>
+                    <option key={dataKeys[18]} value={dataKeys[18]}>{dataKeys[18]}</option>
+                    <option key={dataKeys[17]} value={dataKeys[17]}>{dataKeys[17]}</option>
+                    <option key={dataKeys[24]} value={dataKeys[24]}>{dataKeys[24]}</option>
+                  {/* {dataKeys.map((key) => (
+                    <option key={key} value={key}>
+                      {key}
+                    </option>
+                  ))} */}
+                </select>
+              </div>
+              <div className="text-center my-3"><input type="checkbox" onClick={(e) => setScatoLine(e.target.checked)}></input>    Average Line</div>
+              <div className="text-center my-3"><input type="checkbox" onClick={(e) => setScatoBg(e.target.checked)}></input>    Background Color</div>
+              <div className="rounded text-center mx-20 my-10 h-20 bg-gray-200"></div>
+            </div>
+            
           </div>
         </div>
       </div>
     </>
-  );
-}
-
-function HeaderText({ children }) {
-  return (
-    <h2 className="my-8 text-4xl text-center font-medium leading-10">
-      {children}
-    </h2>
   );
 }
 
--- front-end/src/graphs/ScatterPlot.js
+++ front-end/src/graphs/ScatterPlot.js
@@ -1,63 +1,184 @@
 import React from "react";
 import Plot from "react-plotly.js";
-import { dataNonGeo } from "../js/utils/MockData";
+import { dataNonGeo } from "../js/utils/MockData-Winny.js";
 
-const ScatterPlot = ({ data, xAxisAttribute, yAxisAttribute }) => {
-  let xData = data.map((d) => {
+let randomColors= 
+  ["#1bb699", "#c79bc2", "#d02e29", "#1ae6db", "#3e464c", "#a84a8f",
+  "#911e7e", "#3f16d9", "#0f525f", "#ac7c0a", "#b4c086", "#c9d730", "#30cc49",
+  "#3d6751", "#fb4c03", "#640fc1", "#62c03e", "#d3493a", "#88aa0b", "#406df9",
+  "#615af0", "#4be47", "#2a3434", "#4a543f", "#79bca0", "#a8b8d4", "#00efd4",
+  "#7ad236", "#7260d8", "#1deaa7", "#06f43a", "#823c59", "#e3d94c", "#dc1c06",
+  "#f53b2a", "#b46238", "#2dfff6", "#a82b89", "#1a8011", "#436a9f", "#1a806a"];
+
+const ScatterPlot = ({ data, xAxisAttribute, yAxisAttribute, colorAttribute, lineAttribute, backgroundAttribute }) => { 
+
+//<-------get a list of non-numeric variable values, non-repeated------>
+  let colorData = data.map((d, i) => {
+    if (d[colorAttribute] === undefined || d[colorAttribute] === "NULL") {
+      return d[0];
+    }
+    return d[colorAttribute];
+  });
+
+  function getValues(a){
+    var array = [];
+    for (var i = 0; i < a.length; i++) {
+        if (array.indexOf(a[i]) === -1) {
+            array.push(a[i]);
+        }
+    }
+    return array;
+  }
+
+  var values = getValues(colorData); 
+  // console.log(colorData);
+  // console.log(values)
+
+  // function checkValue(a){
+  //     return a == colorData[j];
+  // }
+
+  // let fillColor = [];
+  // for (var j = 0; j < colorData.length; j++) {
+  //   var index = values.findIndex(checkValue)
+  //   fillColor.push(randomColors[index]);
+  // }
+//<-------get a list of non-numeric variable values, non-repeated------>
+
+  let xData;
+  let yData;
+
+  let traces = values.map((key, i) => {
+    xData = data
+      .filter((d) => d[colorAttribute] === key)
+      .map((d) => d[xAxisAttribute]);
+
+    yData = data
+      .filter((d) => d[colorAttribute] === key)
+      .map((d) => d[yAxisAttribute]);
+  
+    colorData = data
+      .filter((d) => d[colorAttribute] === key)
+      .map((d) => d[colorAttribute]);
+
+    return {
+      x: xData,
+      y: yData,
+      text: colorData,
+      mode: "markers",
+      type: "scatter",
+      name: key.toString(),
+      marker: {
+        size: 7,
+        color: randomColors[i],
+        opacity: 0.3
+      },
+      hovertemplate:
+        "<b>" + xAxisAttribute + "</b>: %{x}" +
+        "<br><b>" + yAxisAttribute + "</b>: %{y}<br>" +
+        "<b>" + colorAttribute + "</b>: %{text}"
+        + "<extra></extra>",
+    };
+  });
+  
+
+  //<------new arrays for sorting x/y data > get min/max------>
+  let xDataSorted = data.map((d, i) => {
     if (d[xAxisAttribute] === undefined || d[xAxisAttribute] === "NULL") {
-      return "UNKOWN";
+      return "UNKNOW";
     }
     return d[xAxisAttribute];
   });
-
-  let yData = data.map((d) => {
+  let yDataSorted = data.map((d, i) => {
     if (d[yAxisAttribute] === undefined || d[yAxisAttribute] === "NULL") {
-      return "UNKOWN";
+      return "UNKNOW";
     }
     return d[yAxisAttribute];
   });
+  function myFunction(total, value, index, array) {
+    return total + value;
+  }
+  let xAverage = xDataSorted.reduce(myFunction) / xDataSorted.length;
+  let yAverage = xDataSorted.reduce(myFunction) / yDataSorted.length;
+  xDataSorted.sort(function(a, b){return a-b});
+  yDataSorted.sort(function(a, b){return a-b});
+   //<------new arrays for sorting x/y data > get min/max------>
 
-  var datapoints = {
-    x: xData,
-    y: yData,
-    text: [],
-    mode: "markers",
-    type: "scatter",
-    marker: {
-      size: 12,
-      color: "rgba(90, 201, 203, 0.5)",
-    },
-    hovertemplate:
-      "<b>Age</b>: %{y}" +
-      "<br><b>Immigration Year</b>: %{x}<br>" +
-      "<extra></extra>",
-  };
 
-  var data = [datapoints];
 
+  function show(l){
+    if (l == true){
+      return 1;
+    } else {
+      return 0;
+    }
+  }
   let layout = {
-    margin: {
-      pad: 5,
-    },
-    transition: {
-      duration: 500,
-      easing: "cubic",
-    },
-    frame: {
-      duration: 500,
-    },
-    xaxis: { showgrid: false },
-    yaxis: { showgrid: false },
-    line: {
-      width: 0,
-    },
-    title: "Scatter Plot Graph",
-    hoverlabel: { bgcolor: "#FFF" },
-  };
+      height: 600,
+      margin: {
+        pad: 5,
+      },
+      // transition: {
+      //   duration: 500,
+      // },
+      showlegend: true,
+      xaxis: { 
+        // range: [xDataSorted[0]-2, xDataSorted[xDataSorted.length-1]+2],
+        // dtick: 5,
+        showgrid: true,
+        showticklabels: true },
+      yaxis: { 
+        // range: [yDataSorted[0]-2, yDataSorted[yDataSorted.length-1]+2],
+        // dtick: 5,
+        showgrid: true,
+        showticklabels: true },
+      title: "Scatter Plot Graph",
+      hoverlabel: { bgcolor: "#FFF" },
+      shapes: [
+        {
+          type: 'line',
+          name: 'Average Line',
+          x0: xAverage,
+          y0: 0,
+          x1: xAverage,
+          y1: yDataSorted[yDataSorted.length-1],
+          line: {
+            color: '#fec89a',
+            width: 2,
+          },
+          opacity: show(lineAttribute)*0.8
+        },
+        {
+          type: 'line',
+          name: 'Average Line',
+          x0: 0,
+          y0: yAverage,
+          x1: xDataSorted[xDataSorted.length-1],
+          y1: yAverage,
+          line: {
+            color: '#fec89a',
+            width: 2,
+          },
+          opacity: show(lineAttribute)*0.8
+        },
+        {
+          type: 'rect',
+          x0: xAverage,
+          y0: yAverage,
+          x1: xDataSorted[xDataSorted.length-1],
+          y1: yDataSorted[yDataSorted.length-1],
+          fillcolor: '#fec89a',
+          opacity: show(backgroundAttribute)*0.1,
+          line: {
+            width: 0
+          }
+      },
+      ]
+    };
 
   return (
     <>
-      <Plot className="" data={data} layout={layout} />
+      <Plot className="" data={traces} layout={layout} />
     </>
   );
 };
