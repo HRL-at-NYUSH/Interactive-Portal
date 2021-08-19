1fd007392056316c360e70ee877089aa9258a260
Kai Elwood-Dieu <kaielwooddieu@gmail.com>
2021-08-19 13:05:23 +0200

    added basic outline

--- front-end/src/graphs/Correlation.js
+++ front-end/src/graphs/Correlation.js
@@ -2,6 +2,42 @@ import React from 'react'
 import Plot from 'react-plotly.js'
 
 function Correlation() {
+
+// intake data, and for each element find the correlation between that element and the other element
+// save this data to a "z index", then save different values as x and y 
+
+// for ( var i = 0; i < yValues.length; i++ ) {
+//   for ( var j = 0; j < xValues.length; j++ ) {
+//     var currentValue = zValues[i][j];
+//     if (currentValue != 0.0) {
+//       var textColor = 'white';
+//     }else{
+//       var textColor = 'black';
+//     }
+//     var result = {
+//       xref: 'x1',
+//       yref: 'y1',
+//       x: xValues[j],
+//       y: yValues[i],
+//       text: zValues[i][j],
+//       font: {
+//         family: 'Arial',
+//         size: 12,
+//         color: 'rgb(50, 171, 96)'
+//       },
+//       showarrow: false,
+//       font: {
+//         color: textColor
+//       }
+//     };
+//     layout.annotations.push(result);
+//   }
+// }
+
+// Plotly.newPlot('myDiv', data, layout);
+
+
+
   return (
     <>
       <div>
@@ -10,6 +46,7 @@ function Correlation() {
           <p className="full-text">TBH</p>
         </div>
       </div>
+      <Plot className="" data={traces} layout={layout} />
     </>
   )
 }
