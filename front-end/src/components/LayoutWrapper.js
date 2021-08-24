1655f71cd33f3e21cce24619d0a9aefbce61e9f7
yz3440 <yufeng-zhao@outlook.com>
2021-08-24 00:48:54 -0400

    added margin at 2xl width

--- front-end/src/components/LayoutWrapper.js
+++ front-end/src/components/LayoutWrapper.js
@@ -7,7 +7,9 @@ const LayoutWrapper = ({ children }) => {
     <>
       <div className='flex flex-col justify-between w-full h-screen'>
         <NavBar isFixed={false} />
-        <main className='px-4 sm:px-6 xl:px-8 mb-auto'>{children}</main>
+        <main className='px-4 sm:px-6 xl:px-8 2xl:mx-32 mb-auto'>
+          {children}
+        </main>
         <Footer />
       </div>
       <FloatingFAQ />
